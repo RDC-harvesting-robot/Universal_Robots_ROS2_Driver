0e8ff0909cbb8160c888d048b9677863e1592a68
abe masaki <abe.masaki@sec.co.jp>
2025-08-07 00:10:29 +0900

    fix Moveitでur_dualを参照するための修正

--- ur_moveit_config/launch/ur_moveit.launch.py
+++ ur_moveit_config/launch/ur_moveit.launch.py
@@ -359,8 +359,7 @@ def generate_launch_description():
     declared_arguments.append(
         DeclareLaunchArgument(
             "description_package",
-            # default_value="ur_dual_description",
-            default_value="ur_description",
+            default_value="ur_dual_description",
             description="Description package with robot URDF/XACRO files. Usually the argument "
             "is not set, it enables use of a custom description.",
         )
@@ -368,8 +367,7 @@ def generate_launch_description():
     declared_arguments.append(
         DeclareLaunchArgument(
             "description_file",
-            # default_value="ur_dual_real.urdf.xacro",
-            default_value="ur.urdf.xacro",
+            default_value="ur_dual_real.urdf.xacro",
             description="URDF/XACRO description file with the robot.",
         )
     )
